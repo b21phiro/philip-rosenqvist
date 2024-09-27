a12c29113d661852fe0a703033c205872d2b0f18
b21phiro <philip.rosenqvist@outlook.com>
2024-09-27 11:27:40 +0200

    Init

--- src/core/layout/footer.ts
+++
@@ -1,9 +0,0 @@
-import './../../assets/css/layout/footer.css';
-
-export default (): string => {
-    return `
-        <footer id="footer">
-            <h4>Philip Rosenqvist</h4>
-            <p>This site was built while listening to some very heavy metal. Sorry for the sass, I’m usually normal.</p>
-        </footer>`;
-}
--- src/core/layout/header.ts
+++
@@ -1,9 +0,0 @@
-import './../../assets/css/layout/header.css';
-import Logo from "../module/logo.ts";
-
-export default (): string => {
-    return `
-        <header id="header">
-            ${ Logo() }
-        </header>`;
-}
