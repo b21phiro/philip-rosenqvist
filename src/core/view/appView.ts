5717273c02b66714c9ecf4158d77d65f642064ab
b21phiro <philip.rosenqvist@outlook.com>
2025-04-07 22:36:33 +0200

    init

--- src/core/view/appView.ts
+++
@@ -1,7 +0,0 @@
-import './../../assets/css/layout/app.css';
-import Header from "../layout/header.ts";
-import Footer from "../layout/footer.ts";
-
-export default (routeView: string): string => {
-    return `${ Header() }<main id="main">${ routeView }</main>${ Footer() }`;
-}
