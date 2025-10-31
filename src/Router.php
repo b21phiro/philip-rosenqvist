c959a54f39d35bf2b70ec0fb4d5026bc22dee507
b21phiro <philip.rosenqvist@outlook.com>
2025-10-31 08:07:39 +0100

    Extracted common functionality into router

---
+++ src/Router.php
@@ -0,0 +1,70 @@
+<?php namespace Phro\Web;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Phro\Web\Http\Route;
+
+class Router {
+
+    private array $routes = [];
+
+    public function handle(Request $request): Response {
+        try {
+            $result = $this->findRoute($request);
+            if (!$result) {
+                return new Response(404, [], 'Not found');
+            }
+            return $this->callRouteHandler($result);
+        } catch (\Exception $exception) {
+            return new Response(500, [], '<h1>Internal server error</h1>');
+        }
+
+    }
+
+    public function addRoute(Route $route): void {
+        $this->routes[] = $route;
+    }
+
+    protected function callRouteHandler(array $result): Response {
+        $route = $result[0];
+        $args = array_map(function($arg) {
+            if (filter_var($arg, FILTER_VALIDATE_INT)) {
+                return (int) $arg;
+            } else {
+                return $arg;
+            }
+        }, $result[1]);
+        $controllerName = $route->getHandler()[0];
+        $controller = new $controllerName;
+        $controllerMethod = $route->getHandler()[1];
+        return call_user_func_array([new $controller, $controllerMethod], [...$args]);
+    }
+
+    protected function findRoute(Request $request): array | null {
+        $result = [];
+        for ($i = 0; $i < count($this->routes); ++$i) {
+            $route = $this->routes[$i];
+            if ($route->getMethod() !== $request->getMethod()) continue;
+            if ($params = $route->getParams()) {
+                $path = $route->getPath();
+                $pattern = preg_quote($path, '/');
+                foreach ($params as $param => $type) {
+                    if ($type === 'int') {
+                        $pattern = str_replace('\:'.$param, '(\d+)', $pattern);
+                    } else if ($type === 'string') {
+                        $pattern = str_replace('\:'.$param, '(\w+)', $pattern);
+                    }
+                }
+                if (preg_match('/^'.$pattern.'$/i', $request->getUri()->getPath(), $matches)) {
+                    $result = [$route, array_slice($matches, 1)];
+                    break;
+                }
+            } else if ($route->getPath() === $request->getUri()->getPath()) {
+                $result = [$route, []];
+                break;
+            }
+        }
+        return $result;
+    }
+
+}
