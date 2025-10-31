3bd722d5025ad592906bd28d25a1a110a9b091b9
b21phiro <philip.rosenqvist@outlook.com>
2025-10-31 12:41:44 +0100

    Added basic favicon

--- src/recourses/includes/templates/default.php
+++ src/recourses/includes/templates/default.php
@@ -17,6 +17,7 @@
 <html lang="<?= $lang ?>">
     <head>
         <title><?= $title; ?></title>
+        <link rel="icon" type="image/png" href="public/favicon.ico"/>
     </head>
     <body>
         <?= $content; ?>
