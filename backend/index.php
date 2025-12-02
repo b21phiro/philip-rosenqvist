f3156834785158f9210c9b04346e4608bc62bf84
Philip Rosenqvist <philip.rosenqvist@outlook.com>
2025-12-02 21:27:26 +0100

    Initial commit

--- backend/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$app = new Phro\Web\App();
-$app->run();
--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: '/public/',
-    build: {
-        outDir: '../backend/public',
-        emptyOutDir: true,
-    }
-});
