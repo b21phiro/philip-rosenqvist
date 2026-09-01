a5f21b53478d81f462962ed133319d913cf0cb38
Philip Rosenqvist <philip.rosenqvist@outlook.com>
2026-09-01 19:38:18 +0200

    initialized threejs scene

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,86 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="js">
+    import * as THREE from 'three';
+    import { onMount } from "svelte";
+
+    /** @type {HTMLCanvasElement | undefined} */
+    let canvas = $state();
+    let hero = $derived(canvas?.parentElement);
+
+    /** @type {THREE.WebGLRenderer} */
+    let renderer;
+
+    /** @type {THREE.Scene} */
+    let scene;
+
+    /** @type {THREE.PerspectiveCamera} */
+    let camera;
+
+    let canvasAspectRatio = $derived(() => {
+       return renderer.domElement.width / renderer.domElement.height;
+    });
+
+    function resizeCanvas() {
+        if (!hero) return;
+        const css = getComputedStyle(hero);
+        const width = parseInt(css.width);
+        const height = parseInt(css.height);
+        renderer.setSize(width, height);
+        camera.aspect = canvasAspectRatio();
+        camera.updateProjectionMatrix();
+    }
+
+    onMount(() => {
+
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const cube = new THREE.Mesh( geometry, material );
+
+        scene = new THREE.Scene();
+        scene.add(cube);
+
+        renderer = new THREE.WebGLRenderer({ canvas });
+
+        camera = new THREE.PerspectiveCamera(75, renderer.domElement.width / renderer.domElement.height, 0.1, 1000);
+        camera.position.z = 5;
+        resizeCanvas();
+
+        renderer.setAnimationLoop(() => {
+
+            // Update
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            // Draw
+            renderer.render(scene, camera);
+        });
+
+    });
+
+</script>
+
+<!-- Hero -->
+<div class="hero">
+
+    <canvas bind:this={canvas}></canvas>
+    
+</div>
+
+<svelte:window on:resize={() => resizeCanvas()} />
+
+<style>
+
+    .hero {
+        position: relative;
+        width: 100vw;
+        height: 98vh;
+        background-color: #000;
+    }
+
+    .hero canvas {
+        position: absolute;
+        inset: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
