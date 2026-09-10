d46b0c9bf910a89cd34315f48a0183dbab8f1558
Philip Rosenqvist <philip.rosenqvist@outlook.com>
2026-09-10 19:10:07 +0200

    Prepared blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,2 +1,19 @@
-<script lang="ts"></script>
+<script lang="ts">
+</script>
 
+<div class="site">
+    <div class="site-wrapper">
+        <h1 class="site-title">Blog</h1>
+    </div>
+</div>
+
+<style>
+    .site {
+        padding: 6rem 1rem;
+    }
+
+    .site-title {
+        font-size: 3rem;
+    }
+
+</style>
