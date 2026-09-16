fc879ece576ace7983fc0edc0445650cb405764c
Philip Rosenqvist <philip.rosenqvist@outlook.com>
2026-09-16 07:02:56 +0200

    Wrote about me

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,136 @@
 <script lang="ts">
+
+    import { ExternalLinkIcon } from '@lucide/svelte';
+
 </script>
 
 <div class="site">
 
-    <div class="site-wrapper">
+    <div class="site-wrapper text-wrapper">
 
-        <p>About</p>
+        <h1 class="site-title">About me</h1>
+
+        <p>
+            Hello, my name is Philip Rosenqvist. I’m 28 years old and live in Stockholm. I’ve been coding since 2018,
+            got my bachelor’s degree in 2024, and began working professionally in 2025,
+            although I did freelance work during my studies.
+        </p>
+
+        <p>
+            I love creating software with code, and I’m currently pursuing my dream of developing my own game. You can
+            read about the development of my game <a href="/blog">here on my blog</a>.
+        </p>
+
+        <p>
+            My favourite music is metal, metalcore, and deathcore, although I also love punk and rap.
+            Anything that slaps, really and make you say “oh, damn”.
+        </p>
+
+        <h2>So, why am I doing this?</h2>
+
+        <p>
+            When I was 10 years old, there was this game called
+            <a href="https://littlebigplanet.fandom.com/wiki/LittleBigPlanet">Little Big Planet
+                <ExternalLinkIcon size="16" aria-hidden="true" />
+            </a>, developed by the original team at
+            <a href="https://www.mediamolecule.com/">
+                Media Molecule <ExternalLinkIcon size="16" aria-hidden="true" />
+            </a>. In short, the game was about creating things by designing platform games, puzzles,
+            and really anything you could come up with. Media Molecule encouraged their players to be creative.
+            I was hooked.
+        </p>
+
+        <p>
+            I didn’t understand it at the time, but I later realised that I was, in fact, programming games within
+            that game. Although the syntax was more physical, it was programming nevertheless.
+            I knew from then on that I wanted to develop games one day.
+        </p>
+
+        <h2>During school</h2>
+
+        <p>
+            In Sweden, after high school, you choose a programme that you want to study in “gymnasiet.” I chose the
+            technical programme because I wanted to become a game developer. However, my time in school was very
+            difficult due to reasons outside of school, and I felt that I needed to take a break. So, I took a year
+            off and chose a new direction, which was social science (a.k.a. Samhällsvetenskapsprogrammet).
+        </p>
+
+        <p>
+            I did enjoy the studies, but I knew they weren’t really what I wanted to do. However, by that point, I had
+            already given up on the game development path because I honestly thought that I wasn’t smart enough to
+            pursue it. So, during my studies, I pursued a military career instead.
+        </p>
+
+        <p>
+            In 2014, I went to a military boot camp for teenagers in Boden, way up north in Sweden, for two weeks.
+            I loved the camaraderie and being out in nature, so I believed that I had found my new calling. After that,
+            I joined “Försvarsutbildarna,” which is essentially the Scouts, but within the military. At this point,
+            I was determined to become a soldier.
+        </p>
+
+        <p>
+            Around my gymnasiet graduation, I had to decide whether I wanted to do the basic military training
+            programme (GU, as it was called back then). I declined, however, so once again I had to find something
+            else to do. The reason I declined was that although I loved the camaraderie and being out in nature,
+            I despised the idea of having to shoot and kill someone. So, I couldn’t do it.
+        </p>
+
+        <h2>Post high-school graduation</h2>
+
+        <p>
+            After graduating in 2018, I did a bunch of small jobs here and there. Most of the time, I worked part-time
+            as a preschool teacher. One day, however, during a sick day, I was bored out of my mind. Like, beyond bored.
+            I was also slightly lost when it came to what I actually wanted to do with my life.
+        </p>
+
+        <p>
+            I began daydreaming and thought about LittleBigPlanet again, and how much I loved that game. I then started
+            thinking about programming again. I had my little MacBook that I had bought from school, and I enrolled in
+            an online programming course.
+        </p>
+
+        <p>
+            I began with HTML, CSS, and JavaScript. I then moved on to PHP and eventually learned about relational
+            databases with MySQLi. Although it wasn’t game development, I had finally found something
+            that I was genuinely good at and, most importantly, had fun doing.
+        </p>
+
+        <h2>During and after my bachelor's programme</h2>
+
+        <p>
+            Between 2019 and 2021, I worked in different fields, but mainly in the steel industry. I tried applying to
+            schools, but I was rejected every year. So, I started working on improving my grades and studied
+            mathematics, science, and everything else I needed. Finally, in 2022, I got into the Web Programming
+            programme in Skövde.
+        </p>
+
+        <p>
+            During that time, I began freelancing in web development, creating full-stack websites.
+            I also started designing UI and UX. I graduated in 2024 and, shortly afterwards,
+            began working full-time as a web developer.
+        </p>
+
+        <h2>Finally begun what I want</h2>
+
+        <p>
+            But my itch for game development has always been there. Eventually, I said,
+            “Fuck it, let’s make a game then.”.
+        </p>
+
+        <p>
+            So, I picked up Programming: Principles and Practice Using C++ by
+            <a href="https://www.stroustrup.com/">
+                Bjarne Stroustrup <ExternalLinkIcon size="18" />
+            </a> and began learning C++.
+            I then started practising pixel art, and I’m now finally ready to develop my own game.
+        </p>
 
     </div>
 
 </div>
+
+<style>
+    .text-wrapper {
+        max-width: 40rem;
+    }
+</style>
